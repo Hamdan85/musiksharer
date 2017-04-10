5faddf7709b6ee7a56494716bccbbd73639d0c59
Gabriel Hamdan <ghamdan.eng@gmail.com>
2017-04-10 17:28:51 -0300

    search action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,23 @@
 class PagesController < ApplicationController
   def index
+    @tracks = Track.all.order(:created_at).limit(8).reverse
+    @new_songs = Track.where(created_at: new_period).limit(8)
+    @top_songs = Track.where(created_at: new_period).limit(8)
+  end
+
+  def search
+    @query = params[:query]
+
+    @results = MusicSearcher.search(@query)
   end
 
   def profile
     @user = User.find(params[:id]) || current_user
+  end
+
+  private
+
+  def new_period
+    (Date.today.at_beginning_of_day - 1.week)..Time.now
   end
 end
